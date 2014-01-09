3f92a3c880d32b2122fd3f77172dbbff348fb337
Vladimir Yarotsky <v.yarotsky@itransition.com>
2014-01-09 22:13:41 +0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'nginx'
+maintainer       'Vladimir Yarotsky'
+maintainer_email 'vladimir.yarotsky@gmail.com'
+license          'MIT'
+description      'Installs/Configures nginx'
+version          '0.0.1'
+
+depends "apt"
